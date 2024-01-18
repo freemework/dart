4f2f31bd46863bd5ae70a367a51be183099b3cf5
Max Anurin <maxim.anurin@zxteam.org>
2024-01-18 14:42:17 +0200

    Provide solution for independent package info.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import { FModuleVersionGuard } from "@freemework/common";
+
+import { packageInfo } from "./package_info.js";
+FModuleVersionGuard(packageInfo);
+
 export * from "./f_decimal.extension.js";
 export { FDecimalBackendBigNumber } from "./f_decimal_backend_big_number.js";
